c78f3acec81fb8fa159fc8d3902539ddd80841e6
Rich Siegel <rsiegel@ise.com>
2012-12-17 15:58:30 -0500

    initial commit for cluster resource type
    this is a probably non functional, non tested end of day commit
    to preserve state in version control :)
    
    contains resource types for
    ip
    name
    fileshare
    generic svc
    physical disk
    genapp
    
    commonly used paramater declarations for above resource types
    preliminary validation logic for some resources

---
+++ puppet/lib/type/mscs_resource.rb
@@ -0,0 +1,169 @@
+Puppet::Type.newtype(:mscs_resource) do
+  @doc = "Microsoft Cluster Server resource configuration information"
+
+  ensurable
+
+  newparam(:name) do
+    desc "The name of the cluster resource"
+    isnamevar
+
+    validate do |value|
+      raise Puppet::Error, "Name must not contain whitespace: #{value}" if value =~ /\s/
+      raise Puppet::Error, "Name must not be empty" if value.empty?
+    end
+
+  end
+
+  newparam(:resourcetype, :parent => Puppet::MSCSProperty) do
+    desc "The resource type to be managed by the cluster"
+    newvalues (:ipaddress, :networkname, :fileshare, :genericservice, :physicaldisk, :genericapplication)
+    aliasvalue :ip, :ipaddress
+    aliasvalue: nn, :networkname
+    aliasvalue: fs, :fileshare
+    aliasvalue :gensvc, :genericservice
+    aliasvalue: pd, :physicaldisk
+    aliasvalue :genapp, :genericapplication
+  end
+
+  # ip address params
+  newparam(:address, :parent => Puppet::MSCSProperty) do
+    desc "The ip address to be be assigned to ip address resource"
+  end
+  
+  newparam(:subnetmask, :parent => Puppet::MSCSProperty) do
+    desc "The subnetmask to be be assigned to ip address resource"
+  end
+  
+  newparam(:network, :parent => Puppet::MSCSProperty) do
+    desc "The default gateway to be be assigned to ip address resource"
+  end
+  
+  newparam(:enablenetbios, :parent => Puppet::MSCSProperty) do
+    "The default gateway to be be assigned to ip address resource"
+    defaultto :true
+  end
+  
+  
+  # network name params
+  
+  newparam(:remappipenames, :parent => Puppet::MSCSProperty) do
+    desc "The network name to be be assigned to network name resource"
+  end
+
+  #file share params
+
+  newparam(:path, :parent => Puppet::MSCSProperty) do
+    desc "The network name to be be assigned to file share resource"
+     validate do |value|
+      raise Puppet::Error.new('Must be specified using an absolute path.') unless absolute_path?(value)
+      raise Puppet::Error, "Invalid value for attribute '#{name}', must use back slashes instead of forward slashes" if self.should_to_s(value).include?('/')
+    end
+  end
+
+
+  newparam(:sharename, :parent => Puppet::MSCSProperty) do
+    desc "The sharename to be be assigned to file share  resource"
+  end
+
+
+  newparam(:remark, :parent => Puppet::MSCSProperty) do
+    desc "The remark to be be assigned to file share resource"
+  end
+
+  newparam(:sharedubdirs, :parent => Puppet::MSCSProperty) do
+    desc "Share subdirectory for file share resource? [y/n]"
+    newvalues (:true, :false)
+    aliasvalue (:yes, :true)
+    aliasvalue (:y, :true)
+    aliasvalue (:n, :false)
+    aliasvalue (:no, :false)
+    defaultto :true
+  end
+
+
+  # generic service parameters
+
+  newparam(:startupparameters, :parent => Puppet::MSCSProperty) do
+    desc "The startup parameters to be be assigned to generic service resource"
+    raise Puppet::Error, "Invalid value for attribute '#{name}', must use back slashes instead of forward slashes" if self.should_to_s(value).include?('/')
+  end
+  
+  newparam(:sharename, :parent => Puppet::MSCSProperty) do
+    desc "The sharename to be assigned to generic service resource"
+  end
+
+
+  # physical disk parameters
+  
+  newparam(:signature, :parent => Puppet::MSCSProperty) do
+    desc "The disk signature to be be assigned to physical disk resource"
+  end
+  
+  newparam(:skipchkdsk, :parent => Puppet::MSCSProperty) do
+    desc "Run skipchkdsk on physical disk resource"
+    newvalues (:true, :false)
+    aliasvalue (:yes, :true)
+    aliasvalue (:y, :true)
+    aliasvalue (:n, :false)
+    aliasvalue (:no, :false)
+    defaultto :true
+  end
+  
+  newparam(:conditionalmount, :parent => Puppet::MSCSProperty) do
+    desc "The conditionalmount to be be assigned to physical disk resource"
+  end
+
+  # generic apps properties
+  newparam(:commandline, :parent => Puppet::MSCSProperty) do
+    desc "The commandline to be be assigned to generic app resource"
+  end
+
+  newparam(:currentdirectory, :parent => Puppet::MSCSProperty) do
+    desc "The Currentdirectory to be be assigned to generic app resource"
+    raise Puppet::Error.new('Must be specified using an absolute path.') unless absolute_path?(value)
+    raise Puppet::Error, "Invalid value for attribute '#{name}', must use back slashes instead of forward slashes" if self.should_to_s(value).include?('/')
+  end
+
+  newparam(:interactwithDesktop, :parent => Puppet::MSCSProperty) do
+    desc "The InteractWithDesktop to be be assigned to generic app resource"
+    newvalues (:true, :false)
+    aliasvalue (:yes, :true)
+    aliasvalue (:y, :true)
+    aliasvalue (:n, :false)
+    aliasvalue (:no, :false)
+    defaultto :true
+  end
+
+  newparam(:usenetworkname, :parent => Puppet::MSCSProperty) do
+    desc "The UseNetworkName generic app resource"
+    newvalues (:true, :false)
+    aliasvalue (:yes, :true)
+    aliasvalue (:y, :true)
+    aliasvalue (:n, :false)
+    aliasvalue (:no, :false)
+    defaultto :true
+  end
+  
+
+  validate do
+
+    is_type = !self[:resourcetype]
+
+    case is_type
+      when :ipaddress
+        has_address = !self[:address].nil?
+        has_subnetmask = !self[:subnetmask].nil?
+        self.fail "You must specify address, and subnetmask" if has_address != has_subnetmask
+      when :networkname
+        has_remappipenames = !self[:has_remappipenames].nil?
+        self.fail "You must specify address, and subnetmask" if has_remappipenames.nil
+      # TODO pending
+      #when :fileshare
+        
+      #when :genericservice
+      #when :physicaldisk
+      #when :genericapplication
+    end
+
+  end
+  
