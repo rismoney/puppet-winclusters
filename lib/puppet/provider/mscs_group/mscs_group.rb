f8301684cac628e5f26400ca9a80693c9d01fd78
Rich Siegel <rsiegel@ise.com>
2013-01-09 12:14:22 -0500

    type and provider created for group mgmt

---
+++ lib/puppet/provider/mscs_group/mscs_group.rb
@@ -0,0 +1,26 @@
+require 'C:/gitrepos/ruby-mscs/lib/mscs.rb'
+
+# fetch the mscs_type call the class provide passing it mscs_resource 
+
+Puppet::Type.type(:mscs_group).provide(:mscs_group) do
+  desc "Group Provider for MSCS clusters." 
+
+  def create
+     cluster_handle=Mscs::Cluster.open('Cluster',@resource[:clustername])
+     Mscs::Group.add(cluster_handle,@resource[:name])
+  end
+
+  def destroy
+    cluster_handle=Mscs::Cluster.open('Cluster',@resource[:clustername])
+    removal=Mscs::Group.remove(cluster_handle,@resource[:name])
+  end
+
+  def exists?
+    cluster_handle=Mscs::Cluster.open('Cluster',@resource[:clustername]) 
+    groupquery=Mscs::Cluster.enumerate('Cluster', cluster_handle, 8)
+    groupquery.include? @resource[:name]
+  end
+
+end
+
+
